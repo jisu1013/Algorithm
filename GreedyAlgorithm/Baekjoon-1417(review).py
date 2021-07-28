3a9b2ac03c85deb496fb78d4a19b2379c3ff2320
Jisu Rho <61077498+jisu1013@users.noreply.github.com>
2021-07-28 14:53:59 +0900

    Update Baekjoon-1417(review).py

--- GreedyAlgorithm/Baekjoon-1417(review).py
+++ GreedyAlgorithm/Baekjoon-1417(review).py
@@ -2,12 +2,15 @@ N = int(input())
 people = []
 result = 0
 dasom = int(input())
-for i in range(N-1):
-  people.append(int(input())) 
+for _ in range(N-1):
+    people.append(int(input())) 
 people = sorted(people, reverse=True)
-while people[0] >= dasom:
-  dasom += 1
-  people[0] -= 1
-  result += 1
-  people = sorted(people, reverse=True)
-print(result)
+if N == 1:
+    print(0)
+else:
+    while people[0] >= dasom:
+        dasom += 1
+        people[0] -= 1
+        result += 1
+        people = sorted(people, reverse=True)
+    print(result)
