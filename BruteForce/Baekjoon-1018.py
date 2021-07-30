be40ecea66f20db6475de37a78ed3c8481b2a468
Jisu Rho <61077498+jisu1013@users.noreply.github.com>
2021-07-30 19:52:20 +0900

    Update Baekjoon-1018.py

--- BruteForce/Baekjoon-1018.py
+++ BruteForce/Baekjoon-1018.py
@@ -1,1 +1,25 @@
-#
+def count(target, start):
+  if target == 0:
+    answer = [0,1,0,1,0,1]
+    
+  else:
+    answer = [1,0,1,0,1,0]
+
+n,m = map(int, input().split())
+board = [[0] * m for _ in range(n)]
+for i in range(n):
+  tmp = input()
+  for j in range(m):
+    if tmp[j] == 'B':
+      board[i][j] = 1
+    else:
+      board[i][j] = 0
+print(board)
+cnt = 64
+for i in range(n-7):
+  for j in range(m-7):
+    if board[i][j] == 0:
+      tmp = count(0, [i,j])
+    else:
+      tmp = count(1, [i,j])    
+    cnt = min(tmp, cnt)
